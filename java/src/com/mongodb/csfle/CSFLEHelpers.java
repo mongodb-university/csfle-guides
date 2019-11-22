74ae49a75849cfb95b1a4d0b51132c7f0eb1e5b7
Chris Cho <chris.cho@10gen.com>
2019-11-22 14:24:11 -0500

    retab

--- java/src/com/mongodb/csfle/CSFLEHelpers.java
+++ java/src/com/mongodb/csfle/CSFLEHelpers.java
@@ -47,173 +47,173 @@ import com.mongodb.client.vault.ClientEncryptions;
  * Helper methods and sample data for this companion project.
  */
 public class CSFLEHelpers {
-	
-	// Sample data
-	public static String SAMPLE_NAME_VALUE = "John Doe";
-	public static Integer SAMPLE_SSN_VALUE = 145014000;
-	
-	public static Document SAMPLE_DOC = new Document()
-			.append("name", SAMPLE_NAME_VALUE)
-			.append("ssn", SAMPLE_SSN_VALUE)
-			.append("bloodType", "AB-")
-			.append("medicalRecords", Arrays.asList(
-					new Document().append("weight", "180"),
-					new Document().append("bloodPressure", "120/80")))
-			.append("insurance", new Document()
-				.append("policyNumber", 123142)
-				.append("provider", "MaestCare"));
-	
-	// Reads the 96-byte local master key
-	public static byte[] readMasterKey(String filePath) throws IOException {
-		FileInputStream fis = null;
-		int numBytes = 96;
-		byte[] fileBytes = new byte[numBytes];
 
-		try {
-			fis = new FileInputStream(filePath);
-			if (fis.read(fileBytes) < numBytes)
-				throw new Exception("Expected to read 96 bytes from file");
-		} catch(Exception e) {
-			e.printStackTrace();
-			System.exit(1);;
-		} finally {
-			if (fis != null)
-				fis.close();
-		}
-		return fileBytes;
-	}
+    // Sample data
+    public static String SAMPLE_NAME_VALUE = "John Doe";
+    public static Integer SAMPLE_SSN_VALUE = 145014000;
 
-	// JSON Schema helpers
-	private static String DETERMINISTIC_ENCRYPTION_TYPE = "AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic";
-	private static String RANDOM_ENCRYPTION_TYPE = "AEAD_AES_256_CBC_HMAC_SHA_512-Random";
+    public static Document SAMPLE_DOC = new Document()
+            .append("name", SAMPLE_NAME_VALUE)
+            .append("ssn", SAMPLE_SSN_VALUE)
+            .append("bloodType", "AB-")
+            .append("medicalRecords", Arrays.asList(
+                    new Document().append("weight", "180"),
+                    new Document().append("bloodPressure", "120/80")))
+            .append("insurance", new Document()
+                .append("policyNumber", 123142)
+                .append("provider", "MaestCare"));
 
-	private static Document buildEncryptedField(String keyId, String bsonType, Boolean isDeterministic) {
-		return new Document().
-				append("encrypt", new Document()
-						.append("bsonType", bsonType)
-						.append("algorithm", 
-								(isDeterministic) ? DETERMINISTIC_ENCRYPTION_TYPE : RANDOM_ENCRYPTION_TYPE));
-	}
+    // Reads the 96-byte local master key
+    public static byte[] readMasterKey(String filePath) throws IOException {
+        FileInputStream fis = null;
+        int numBytes = 96;
+        byte[] fileBytes = new byte[numBytes];
 
-	private static Document createEncryptMetadataSchema(String keyId) {
-		List<Document> keyIds = new ArrayList<Document>();
-		keyIds.add(new Document()
-				.append("$binary", new Document()
-						.append("base64", keyId)
-						.append("subType", "04")));
-		return new Document().append("keyId", keyIds);
-	}
+        try {
+            fis = new FileInputStream(filePath);
+            if (fis.read(fileBytes) < numBytes)
+                throw new Exception("Expected to read 96 bytes from file");
+        } catch(Exception e) {
+            e.printStackTrace();
+            System.exit(1);;
+        } finally {
+            if (fis != null)
+                fis.close();
+        }
+        return fileBytes;
+    }
 
-	public static Document createJSONSchema(String keyId) throws IllegalArgumentException {
-		if (keyId.isEmpty()) {
-			throw new IllegalArgumentException("keyId must contain your base64 encryption key id.");
-		}
-		return new Document().append("bsonType", "object").append("encryptMetadata", createEncryptMetadataSchema(keyId))
-				.append("properties", new Document()
-						.append("ssn", buildEncryptedField(keyId, "int", true))
-						.append("bloodType", buildEncryptedField(keyId, "string", false))
-						.append("medicalRecords", buildEncryptedField(keyId, "array", false))
-						.append("insurance", new Document()
-								.append("bsonType", "object")
-								.append("properties",
-										new Document().append("policyNumber", buildEncryptedField(keyId, "int", true)))));
-	}
-	
-	// Creates Normal Client
-	public static MongoClient createMongoClient(String connectionString) {
-		return MongoClients.create(connectionString);
-	}
-	
-	// Creates KeyVault which allows you to create a key as well as encrypt and decrypt fields
-	public static ClientEncryption createKeyVault(String connectionString, String kmsProvider, byte[] localMasterKey, String keyVaultCollection) {
-		Map<String, Object> masterKeyMap = new HashMap<String, Object>();
-		masterKeyMap.put("key", localMasterKey);
-		Map<String, Map<String, Object>> kmsProviders = new HashMap<String, Map<String, Object>>();
-		kmsProviders.put(kmsProvider, masterKeyMap);
-		
-		ClientEncryptionSettings clientEncryptionSettings = ClientEncryptionSettings.builder()
-				.keyVaultMongoClientSettings(MongoClientSettings.builder()
-						.applyConnectionString(new ConnectionString(connectionString))
-						.build())
-				.keyVaultNamespace(keyVaultCollection)
-				.kmsProviders(kmsProviders)
-				.build();
+    // JSON Schema helpers
+    private static String DETERMINISTIC_ENCRYPTION_TYPE = "AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic";
+    private static String RANDOM_ENCRYPTION_TYPE = "AEAD_AES_256_CBC_HMAC_SHA_512-Random";
 
-		return ClientEncryptions.create(clientEncryptionSettings);
-	}
-	
-	// You may need to update the following variable to point to your mongocryptd binary
-	private static String mongocryptdPath = "/usr/local/bin/mongocryptd";
+    private static Document buildEncryptedField(String keyId, String bsonType, Boolean isDeterministic) {
+        return new Document().
+                append("encrypt", new Document()
+                        .append("bsonType", bsonType)
+                        .append("algorithm",
+                                (isDeterministic) ? DETERMINISTIC_ENCRYPTION_TYPE : RANDOM_ENCRYPTION_TYPE));
+    }
 
-	// Creates Encrypted Client which performs automatic encryption and decryption of fields
-	public static MongoClient createEncryptedClient(String connectionString, String kmsProvider, byte[] masterKey, String keyVaultCollection, Document schema, String dataDb, String dataColl) {
-		String recordsNamespace = dataDb + "." + dataColl;
-		
-		Map<String, BsonDocument> schemaMap = new HashMap<String, BsonDocument>();
-		schemaMap.put(recordsNamespace, BsonDocument.parse(schema.toJson())); 
-		
-		Map<String, Object> keyMap = new HashMap<String, Object>();
-		keyMap.put("key", masterKey);
-		
-		Map<String, Map<String, Object>> kmsProviders = new HashMap<String, Map<String, Object>>();
-		kmsProviders.put(kmsProvider, keyMap);
+    private static Document createEncryptMetadataSchema(String keyId) {
+        List<Document> keyIds = new ArrayList<Document>();
+        keyIds.add(new Document()
+                .append("$binary", new Document()
+                        .append("base64", keyId)
+                        .append("subType", "04")));
+        return new Document().append("keyId", keyIds);
+    }
 
-		Map<String, Object> extraOpts = new HashMap<String, Object>();
-		extraOpts.put("mongocryptdSpawnPath", mongocryptdPath);
-		// uncomment the following line if you are running mongocryptd manually
-//		extraOpts.put("mongocryptdBypassSpawn", true);
-		
-		AutoEncryptionSettings autoEncryptionSettings = AutoEncryptionSettings.builder()
-				.keyVaultNamespace(keyVaultCollection)
-				.kmsProviders(kmsProviders)
-				.extraOptions(extraOpts)
-				.schemaMap(schemaMap)
-				.build();
+    public static Document createJSONSchema(String keyId) throws IllegalArgumentException {
+        if (keyId.isEmpty()) {
+            throw new IllegalArgumentException("keyId must contain your base64 encryption key id.");
+        }
+        return new Document().append("bsonType", "object").append("encryptMetadata", createEncryptMetadataSchema(keyId))
+                .append("properties", new Document()
+                        .append("ssn", buildEncryptedField(keyId, "int", true))
+                        .append("bloodType", buildEncryptedField(keyId, "string", false))
+                        .append("medicalRecords", buildEncryptedField(keyId, "array", false))
+                        .append("insurance", new Document()
+                                .append("bsonType", "object")
+                                .append("properties",
+                                        new Document().append("policyNumber", buildEncryptedField(keyId, "int", true)))));
+    }
 
-		MongoClientSettings clientSettings = MongoClientSettings.builder()
-				.applyConnectionString(new ConnectionString(connectionString))
-				.autoEncryptionSettings(autoEncryptionSettings)
-				.build();
+    // Creates Normal Client
+    public static MongoClient createMongoClient(String connectionString) {
+        return MongoClients.create(connectionString);
+    }
 
-		return MongoClients.create(clientSettings);
-	}
+    // Creates KeyVault which allows you to create a key as well as encrypt and decrypt fields
+    public static ClientEncryption createKeyVault(String connectionString, String kmsProvider, byte[] localMasterKey, String keyVaultCollection) {
+        Map<String, Object> masterKeyMap = new HashMap<String, Object>();
+        masterKeyMap.put("key", localMasterKey);
+        Map<String, Map<String, Object>> kmsProviders = new HashMap<String, Map<String, Object>>();
+        kmsProviders.put(kmsProvider, masterKeyMap);
 
-	// Returns existing data encryption key
-	public static String findDataEncryptionKey(String connectionString, String keyAltName, String keyDb, String keyColl) {
-		MongoClient mongoClient = createMongoClient(connectionString);
-		
-		Document query = new Document("keyAltNames", keyAltName);
-		MongoCollection<Document> collection = mongoClient.getDatabase(keyDb).getCollection(keyColl);
-		BsonDocument doc = collection
-				.withDocumentClass(BsonDocument.class)
-				.find(query)
-				.first();
-		
-		if (doc != null) {
-			return Base64.getEncoder().encodeToString(doc.getBinary("_id").getData());
-		}
-		return null;
-	}
-	
-	// Creates index for keyAltNames in the specified key collection
-	public static void createKeyVaultIndex(String connectionString, String keyDb, String keyColl) {
-		MongoClient mongoClient = createMongoClient(connectionString);
-		
-		MongoCollection<Document> collection = mongoClient.getDatabase(keyDb).getCollection(keyColl);
-		
-		Document filterExpr = new Document("keyAltNames", new Document("$exists", true));
-		IndexOptions indexOptions = new IndexOptions().unique(true).partialFilterExpression((Bson) filterExpr);
-		
-		collection.createIndex(new Document("keyAltNames", 1), indexOptions);
-	}
-	
-	// Create data encryption key in the specified key collection
-	// Call only after checking whether a data encryption key with same keyAltName exists
-	public static String createDataEncryptionKey(String connectionString, String kmsProvider, byte[] localMasterKey, String keyVaultCollection, String keyAltName) {
-		ClientEncryption keyVault = createKeyVault(connectionString, kmsProvider, localMasterKey, keyVaultCollection);
+        ClientEncryptionSettings clientEncryptionSettings = ClientEncryptionSettings.builder()
+                .keyVaultMongoClientSettings(MongoClientSettings.builder()
+                        .applyConnectionString(new ConnectionString(connectionString))
+                        .build())
+                .keyVaultNamespace(keyVaultCollection)
+                .kmsProviders(kmsProviders)
+                .build();
 
-		BsonBinary dataKeyId = keyVault.createDataKey(kmsProvider, new DataKeyOptions().keyAltNames(Arrays.asList(keyAltName)));
-		
-		return Base64.getEncoder().encodeToString(dataKeyId.getData());
-	}	
+        return ClientEncryptions.create(clientEncryptionSettings);
+    }
+
+    // You may need to update the following variable to point to your mongocryptd binary
+    private static String mongocryptdPath = "/usr/local/bin/mongocryptd";
+
+    // Creates Encrypted Client which performs automatic encryption and decryption of fields
+    public static MongoClient createEncryptedClient(String connectionString, String kmsProvider, byte[] masterKey, String keyVaultCollection, Document schema, String dataDb, String dataColl) {
+        String recordsNamespace = dataDb + "." + dataColl;
+
+        Map<String, BsonDocument> schemaMap = new HashMap<String, BsonDocument>();
+        schemaMap.put(recordsNamespace, BsonDocument.parse(schema.toJson()));
+
+        Map<String, Object> keyMap = new HashMap<String, Object>();
+        keyMap.put("key", masterKey);
+
+        Map<String, Map<String, Object>> kmsProviders = new HashMap<String, Map<String, Object>>();
+        kmsProviders.put(kmsProvider, keyMap);
+
+        Map<String, Object> extraOpts = new HashMap<String, Object>();
+        extraOpts.put("mongocryptdSpawnPath", mongocryptdPath);
+        // uncomment the following line if you are running mongocryptd manually
+//      extraOpts.put("mongocryptdBypassSpawn", true);
+
+        AutoEncryptionSettings autoEncryptionSettings = AutoEncryptionSettings.builder()
+                .keyVaultNamespace(keyVaultCollection)
+                .kmsProviders(kmsProviders)
+                .extraOptions(extraOpts)
+                .schemaMap(schemaMap)
+                .build();
+
+        MongoClientSettings clientSettings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .autoEncryptionSettings(autoEncryptionSettings)
+                .build();
+
+        return MongoClients.create(clientSettings);
+    }
+
+    // Returns existing data encryption key
+    public static String findDataEncryptionKey(String connectionString, String keyAltName, String keyDb, String keyColl) {
+        MongoClient mongoClient = createMongoClient(connectionString);
+
+        Document query = new Document("keyAltNames", keyAltName);
+        MongoCollection<Document> collection = mongoClient.getDatabase(keyDb).getCollection(keyColl);
+        BsonDocument doc = collection
+                .withDocumentClass(BsonDocument.class)
+                .find(query)
+                .first();
+
+        if (doc != null) {
+            return Base64.getEncoder().encodeToString(doc.getBinary("_id").getData());
+        }
+        return null;
+    }
+
+    // Creates index for keyAltNames in the specified key collection
+    public static void createKeyVaultIndex(String connectionString, String keyDb, String keyColl) {
+        MongoClient mongoClient = createMongoClient(connectionString);
+
+        MongoCollection<Document> collection = mongoClient.getDatabase(keyDb).getCollection(keyColl);
+
+        Document filterExpr = new Document("keyAltNames", new Document("$exists", true));
+        IndexOptions indexOptions = new IndexOptions().unique(true).partialFilterExpression((Bson) filterExpr);
+
+        collection.createIndex(new Document("keyAltNames", 1), indexOptions);
+    }
+
+    // Create data encryption key in the specified key collection
+    // Call only after checking whether a data encryption key with same keyAltName exists
+    public static String createDataEncryptionKey(String connectionString, String kmsProvider, byte[] localMasterKey, String keyVaultCollection, String keyAltName) {
+        ClientEncryption keyVault = createKeyVault(connectionString, kmsProvider, localMasterKey, keyVaultCollection);
+
+        BsonBinary dataKeyId = keyVault.createDataKey(kmsProvider, new DataKeyOptions().keyAltNames(Arrays.asList(keyAltName)));
+
+        return Base64.getEncoder().encodeToString(dataKeyId.getData());
+    }
 }
--- java/src/com/mongodb/csfle/DataEncryptionKeyCreator.java
+++ java/src/com/mongodb/csfle/DataEncryptionKeyCreator.java
@@ -24,29 +24,29 @@ import java.io.IOException;
  * - Locates existing local encryption key from encryption.__keyVault collection
  */
 public class DataEncryptionKeyCreator {
-	
-	public static void main(String[] args) throws IOException {
-		String connectionString = "mongodb://localhost:27017";
-		String keyDb = "encryption";
-		String keyColl = "__keyVault";
-		String keyAltName = "demo-data-key";
-		String kmsProvider = "local";
-		String keyVaultCollection = String.join(".", keyDb, keyColl);
-		
-		// Read the local master key from the provided file
-		byte[] masterKey = CSFLEHelpers.readMasterKey("./master-key.txt");
 
-		// Find or insert a new data data key
-		String encryptionKey = CSFLEHelpers.findDataEncryptionKey(connectionString, keyAltName, keyDb, keyColl);
-		
-		if (encryptionKey == null) {
-			// No key found; create index on key vault and a new encryption key and print the key
-			CSFLEHelpers.createKeyVaultIndex(connectionString, keyDb, keyColl);
-			encryptionKey = CSFLEHelpers.createDataEncryptionKey(connectionString, kmsProvider, masterKey, keyVaultCollection, keyAltName);
-			System.out.println("Congratulations on creating a new data encryption key! Copy the key below and paste it into InsertDataWithEncryptedfields.java\n" + encryptionKey);
-		} else {
-			// Print the key
-			System.out.println("Retrieved your existing key. Copy the key below and paste it into InsertDataWithEncryptedFields.java\n" + encryptionKey);
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        String connectionString = "mongodb://localhost:27017";
+        String keyDb = "encryption";
+        String keyColl = "__keyVault";
+        String keyAltName = "demo-data-key";
+        String kmsProvider = "local";
+        String keyVaultCollection = String.join(".", keyDb, keyColl);
+
+        // Read the local master key from the provided file
+        byte[] masterKey = CSFLEHelpers.readMasterKey("./master-key.txt");
+
+        // Find or insert a new data data key
+        String encryptionKey = CSFLEHelpers.findDataEncryptionKey(connectionString, keyAltName, keyDb, keyColl);
+
+        if (encryptionKey == null) {
+            // No key found; create index on key vault and a new encryption key and print the key
+            CSFLEHelpers.createKeyVaultIndex(connectionString, keyDb, keyColl);
+            encryptionKey = CSFLEHelpers.createDataEncryptionKey(connectionString, kmsProvider, masterKey, keyVaultCollection, keyAltName);
+            System.out.println("Congratulations on creating a new data encryption key! Copy the key below and paste it into InsertDataWithEncryptedfields.java\n" + encryptionKey);
+        } else {
+            // Print the key
+            System.out.println("Retrieved your existing key. Copy the key below and paste it into InsertDataWithEncryptedFields.java\n" + encryptionKey);
+        }
+    }
 }
