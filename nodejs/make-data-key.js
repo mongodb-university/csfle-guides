0031528e0c3d12164c0351593986e44c6b954db1
Nathan Leniz <nathan.leniz@mongodb.com>
2020-12-10 14:01:20 -0800

    use try/finally within main

--- nodejs/make-data-key.js
+++ nodejs/make-data-key.js
@@ -2,23 +2,21 @@ const kms = require("./kms");
 require("dotenv").config();
 
 async function main(unencryptedClient) {
-  const kmsClient = kms.localCsfleHelper();
+  try {
+    const kmsClient = kms.localCsfleHelper();
 
-  unencryptedClient = await kmsClient.getRegularClient();
+    unencryptedClient = await kmsClient.getRegularClient();
 
-  const dataKey = await kmsClient.findOrCreateDataKey(unencryptedClient);
-  console.log(
-    `Base64 data key. Copy and paste this into clients.js:
+    const dataKey = await kmsClient.findOrCreateDataKey(unencryptedClient);
+    console.log(
+      `Base64 data key. Copy and paste this into clients.js:
 
 ${dataKey}
 `
-  );
+    );
+  } finally {
+    await unencryptedClient.close();
+  }
 }
-let unencryptedClient = null;
-main()
-  .catch(console.dir)
-  .finally(async () => {
-    console.log("closing client");
-    if (unencryptedClient) await unencryptedClient.close();
-    console.log("closed client");
-  });
+let unencryptedClient;
+main().catch(console.dir);
