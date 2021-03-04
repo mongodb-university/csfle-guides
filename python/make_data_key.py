682e1d7c34937f7400d63428e83b764a5c2237ca
Nathan Leniz <nathan.leniz@mongodb.com>
2021-03-03 20:58:22 -0800

    use keyAltNames

--- python/make_data_key.py
+++ python/make_data_key.py
@@ -21,6 +21,7 @@ import base64
 from uuid import UUID
 from helpers import read_master_key, CsfleHelper
 
+
 def main():
 
     local_master_key = read_master_key()
@@ -32,8 +33,9 @@ def main():
     }
 
     csfle_helper = CsfleHelper(kms_provider=kms_provider)
-    binary_data_key = csfle_helper.find_or_create_data_key()
-    data_key = base64.b64encode(binary_data_key).decode("utf-8")
+    data_key = csfle_helper.find_or_create_data_key()
+    print(data_key)
+    # data_key = base64.b64encode(binary_data_key).decode("utf-8")
 
     print("Base64 data key. Copy and paste this into app.py\t", data_key)
 
