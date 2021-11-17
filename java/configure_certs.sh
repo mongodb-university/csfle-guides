6f30f34214b1e3ce06bc6c83442857cfe6734632
Aleksander Binion <alek.binion@mongodb.com>
2021-11-17 16:50:01 -0500

    configure password on startup

--- java/configure_certs.sh
+++ java/configure_certs.sh
@@ -1,13 +1,17 @@
 # clone the test kmip server
 git clone https://github.com/mongodb-labs/drivers-evergreen-tools.git
-# generate a pkcs12 file from the ca.pem file in evergreen tools
+
+# key configuration from evergreen
 openssl pkcs12 -CAfile drivers-evergreen-tools/.evergreen/x509gen/ca.pem -export -in drivers-evergreen-tools/.evergreen/x509gen/client.pem -out client.pkc -password pass:${KEYSTORE_PASSWORD}
 cp ${JAVA_HOME}/lib/security/cacerts mongo-truststore
 ${JAVA_HOME}/bin/keytool -importcert -trustcacerts -file drivers-evergreen-tools/.evergreen/x509gen/ca.pem -keystore mongo-truststore -storepass ${KEYSTORE_PASSWORD} -storetype JKS -noprompt
 
+# specify password in maven config file
+cp maven.config.tmpl .mvn/maven.config
+sed -i '' -e "s/REPLACE-WITH-KEYSTORE-PASSWORD/$KEYSTORE_PASSWORD/g" .mvn/maven.config
 
+# start the kmip server
 echo "Starting the KMIP Server..."
-trap "deactivate" INT
 cd drivers-evergreen-tools/.evergreen/csfle
 . ./activate_venv.sh
 ./kmstlsvenv/bin/python3 -u kms_kmip_server.py
