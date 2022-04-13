489fcb4677ee485c22bbf6218e18a89329abfcca
Aleksander Binion <alek.binion@mongodb.com>
2022-04-13 16:44:52 -0400

    use deterministic encryption

--- python/helpers.py
+++ python/helpers.py
@@ -140,8 +140,8 @@ class CsfleHelper:
                             "policyNumber": {
                                 "encrypt": {
                                     "bsonType": "int",
-                                    "keyId":"/key",
-                                    "algorithm": "AEAD_AES_256_CBC_HMAC_SHA_512-Random"
+                                    "keyId":[data_key],
+                                    "algorithm": "AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic"
                                 }
                             }
                         }
@@ -163,8 +163,8 @@ class CsfleHelper:
                     "ssn": {
                         "encrypt": {
                             "bsonType": "int",
-                            "keyId":"/key",
-                            "algorithm": "AEAD_AES_256_CBC_HMAC_SHA_512-Random"
+                            "keyId":[data_key],
+                            "algorithm": "AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic"
                         }
                     }
                 }
